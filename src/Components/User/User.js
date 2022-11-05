f4ff302ce242ae1aaa0c0df7b7396f41c089a46b
regularman171 <regularman171@gmail.com>
2022-11-05 15:25:43 +0530

    using state to save user details

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,21 +1,33 @@
+import {useState} from 'react';
 import styles from './User.module.css';
-
 import Card from "../UI/Card";
 import Button from '../UI/Button';
 
 const User = (props) => {
 
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredAge, enteredUsername)
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value)
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value)
     }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username: </label>
-                <input id="username" type="text" />
+                <input id="username" type="text" onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age: </label>
-                <input id="age" type="number"></input>
+                <input id="age" type="number" onChange={ageChangeHandler}></input>
                 {/* <button type="submit">Add User</button> */}
                 <Button type="submit">
                     Add User
