4e1143f4fc56cd10185df0ebbf74c157263232e2
regularman171 <regularman171@gmail.com>
2022-11-05 15:05:44 +0530

    added card component

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,4 +1,6 @@
-import styles from './User.css';
+import styles from './User.module.css';
+
+import Card from "../UI/Card";
 
 const User = (props) => {
 
@@ -7,13 +9,15 @@ const User = (props) => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username: </label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age: </label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username: </label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age: </label>
+                <input id="age" type="number"></input>
+                <button type="submit">Add User</button>
+            </form>
+        </Card> 
     )
 }
 
